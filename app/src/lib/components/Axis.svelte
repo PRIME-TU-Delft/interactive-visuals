1cf67a5c2a1c39812caab84b3d5b7527d7c14f04
abel (tudelft) <a.debruijn-3@student.tudelft.nl>
2022-08-23 16:06:10 +0200

    Added: axis scaling

--- app/src/lib/components/Axis.svelte
+++ app/src/lib/components/Axis.svelte
@@ -3,20 +3,19 @@
 	import { Scene, Vector3 } from 'three';
 
 	export let scene: Scene;
-	export let hideNumbers = false; //TODO
-	export let hideTicks = false; // TODO
+	// export let hideNumbers = false; //TODO
+	export let hideTicks = false;
 	export let axisLength = 10;
-	export const axisSpacing = 1;
+	export let axisSpacing = 1;
 
-	$: indecators = new Array(axisLength * 2 + 1)
+	$: axisInterval = Math.floor(axisLength / axisSpacing);
+	$: indecators = new Array(axisInterval * 2 + 1)
 		.fill(0)
-		.map((_, i) => (i - axisLength) / axisSpacing); // range -axisLength to including axisLength
+		.map((_, i) => (i - axisInterval) * axisSpacing); // range -axisLength to including axisLength
 	$: largeIndecators = indecators.filter((x, i) => i % (axisSpacing * 2) === 0 && x !== 0); // Even minus 0 indecators
 	$: smallIndecators = indecators.filter((x, i) => i % (axisSpacing * 2) === 1 && x !== 0); // Odd indecators
-	$: largeIndecators && console.log(largeIndecators);
-	$: smallIndecators && console.log(smallIndecators);
 
-	const indecatorSizes = [0.25, 0.125];
+	const tickSizes = [0.25, 0.125]; // Ortigonal lenth of tick
 
 	function getPoints(indecator: number, size: number, axis: number = 0): [Vector3, Vector3] {
 		let from = new Vector3(indecator, 0, size);
@@ -56,14 +55,14 @@
 <!-- Tick indecators -->
 {#if !hideTicks}
 	{#each largeIndecators as indecator}
-		<Line {scene} color="#EF4444" points={getPoints(indecator, indecatorSizes[0])} />
-		<Line {scene} color="#15803D" points={getPoints(indecator, indecatorSizes[0], 1)} />
-		<Line {scene} color="#0EA5E9" points={getPoints(indecator, indecatorSizes[0], 2)} />
+		<Line {scene} color="#EF4444" points={getPoints(indecator, tickSizes[0])} />
+		<Line {scene} color="#15803D" points={getPoints(indecator, tickSizes[0], 1)} />
+		<Line {scene} color="#0EA5E9" points={getPoints(indecator, tickSizes[0], 2)} />
 	{/each}
 
 	{#each smallIndecators as indecator}
-		<Line {scene} color="#EF4444" points={getPoints(indecator, indecatorSizes[1])} />
-		<Line {scene} color="#15803D" points={getPoints(indecator, indecatorSizes[1], 1)} />
-		<Line {scene} color="#0EA5E9" points={getPoints(indecator, indecatorSizes[1], 2)} />
+		<Line {scene} color="#EF4444" points={getPoints(indecator, tickSizes[1])} />
+		<Line {scene} color="#15803D" points={getPoints(indecator, tickSizes[1], 1)} />
+		<Line {scene} color="#0EA5E9" points={getPoints(indecator, tickSizes[1], 2)} />
 	{/each}
 {/if}
--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -5,23 +5,22 @@
 	import getPseudoRandomColor, { colors } from '$lib/utils/getColor';
 
 	import { Vector3 } from 'three';
-	let axisLength = 11;
+	let axisLength = 12;
+	let axisSpacing = 3;
 
 	let color = getPseudoRandomColor([Math.random()]);
 </script>
 
-<input type="number" bind:value={axisLength} />
-<select bind:value={color}>
-	{#each [...colors, '#frd'] as clr}
-		<option value={clr}>
-			{clr}
-		</option>
+<input type="number" bind:value={axisSpacing} />
+<select bind:value={axisLength}>
+	{#each new Array(15) as _, i}
+		<option value={i}>{i}</option>
 	{/each}
 </select>
 
 <Canvas let:scene>
 	<!-- <Cube {scene} /> -->
-	<Vector {scene} {color} points={[new Vector3(0, 5, 0), new Vector3(0, 0, axisLength)]} />
+	<Vector {scene} {color} points={[new Vector3(0, 5, 0), new Vector3(0, 0, -axisLength)]} />
 
-	<Axis {scene} {axisLength} />
+	<Axis {scene} {axisLength} {axisSpacing} />
 </Canvas>
